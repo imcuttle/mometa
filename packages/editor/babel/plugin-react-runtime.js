38bb36891b21a365613d9f454631e46c0590105b
余聪 <yucong@yuanfudao.com>
2021-12-26 17:46:09 +0800

    chore: wip

--- packages/editor/babel/plugin-react-runtime.js
+++ packages/editor/babel/plugin-react-runtime.js
@@ -1,1 +1,6 @@
-module.exports = require('../src/mometa/preset/react/babel/inject')
+const __MOMETA_LOCAL__ = !!process.env.__MOMETA_LOCAL__
+if (__MOMETA_LOCAL__) {
+  module.exports = require('../src/mometa/preset/react/babel/inject')
+} else {
+  module.exports = require('../lib/mometa/preset/react/babel/inject')
+}
