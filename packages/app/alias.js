da62f88659ad17364ee325e9b6fc862e398f8a42
余聪 <yucong@yuanfudao.com>
2022-01-06 11:44:46 +0800

    chore: remove alias

--- packages/app/alias.js
+++
@@ -1,8 +0,0 @@
-const hook = require('require-resolve-hook').default
-
-if (process.env.WEBPACK_VERSION) {
-  const regexp = /^(webpack$|webpack\/.+$)/
-  hook(regexp, (id, p) => {
-    return require.resolve(`webpack${process.env.WEBPACK_VERSION}${id.slice('webpack'.length)}`)
-  })
-}
