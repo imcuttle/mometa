e9f48a81dc5d3ce565fdb21bbb9edca179478e9c
余聪 <yucong.cuttle@bytedance.com>
2022-01-28 15:57:44 +0800

    chore: add demo of style material

--- packages/editor/example/mometa-material.config.js
+++ packages/editor/example/mometa-material.config.js
@@ -2,31 +2,5 @@ module.exports = require('../../app/mometa-material.config').concat([
   {
     name: 'HHHPAC',
     key: 'component'
-  },
-  {
-    name: 'Local Mat',
-    key: 'local-mat',
-    assetGroups: [
-      {
-        name: '通用',
-        key: 'gen',
-        assets: [
-          {
-            name: '按钮',
-            key: 'Button',
-            cover: 'https://gw.alipayobjects.com/zos/alicdn/fNUKzY1sk/Button.svg',
-            data: {
-              code: '<Button type="default">按钮</Button>',
-              dependencies: {
-                Button: {
-                  source: '@/materials/button',
-                  mode: 'default'
-                }
-              }
-            }
-          }
-        ]
-      }
-    ]
   }
 ])
